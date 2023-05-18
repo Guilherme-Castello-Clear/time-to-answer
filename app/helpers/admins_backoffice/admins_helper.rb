228e8e94a45237b25c57d24bf40fd2af75bcccc1
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-05-18 13:03:32 -0300

    Feat: Improved i18n for modules

--- app/helpers/admins_backoffice/admins_helper.rb
+++ app/helpers/admins_backoffice/admins_helper.rb
@@ -1,2 +1,9 @@
 module AdminsBackoffice::AdminsHelper
+    def translate_attribute(object, method)
+        if object && method
+            object.model.human_attribute_name(method)
+        else
+            "Erro: Informe os parâmetros"
+        end
+    end
 end
