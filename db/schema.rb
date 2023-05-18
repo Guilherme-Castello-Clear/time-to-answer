9e8a9bee21b8373e8a6312e60353d7996a00e3ac
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-05-18 12:05:47 -0300

    Feat: Created question list with edit/create/destroy methods

--- db/schema.rb
+++ db/schema.rb
@@ -25,16 +25,16 @@ ActiveRecord::Schema.define(version: 2023_05_18_123440) do
   end
 
   create_table "answers", force: :cascade do |t|
-    t.text "description"
+    t.text "description", null: false
     t.integer "question_id"
-    t.boolean "correct"
+    t.boolean "correct", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
-    t.text "description"
+    t.text "description", null: false
     t.integer "subject_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2023_05_18_123440) do
   end
 
   create_table "subjects", force: :cascade do |t|
-    t.string "description"
+    t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
