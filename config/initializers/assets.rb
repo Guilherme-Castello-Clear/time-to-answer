6b1904e9c01c2fada36345e68130033a9a948b4a
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-05-19 12:39:38 -0300

    Feat: Improved home page layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,10 +15,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css
                                                     users_backoffice.js users_backoffice.css
                                                     admin_devise.scss admin_devise.js
-                                                    user_devise.scss user_devise.js)
+                                                    user_devise.scss user_devise.js
+                                                    site.css site.js)
 
 
 Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css
-                                                custom.js custom.css img.jpg)
+                                                custom.js custom.css img.jpg
+                                                surface-fix.js surface-fix.css
+                                                navbar.css)
 
 Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js )
