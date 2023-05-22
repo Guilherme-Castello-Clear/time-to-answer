b60c23d41844b123378358eeac1acb74469e43dd
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-05-22 09:15:46 -0300

    Fix: Search bar refactored

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,12 @@ class Question < ApplicationRecord
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
+  scope :_search_, ->(page, term){
+    Question.includes(:answers).where("description LIKE ?", "%#{term}%").page(page)
+  }
+
+  scope :last_questions, ->(page){
+    Question.includes(:answers).order('created_at desc').page(page)
+  }
+
 end
