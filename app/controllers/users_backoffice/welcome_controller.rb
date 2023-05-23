b8c6eec64a19777f47746fdde8a5d2ba4c4f9e9e
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-05-23 12:44:28 -0300

    Feat: Added user's answers statistics

--- app/controllers/users_backoffice/welcome_controller.rb
+++ app/controllers/users_backoffice/welcome_controller.rb
@@ -2,6 +2,7 @@ class UsersBackoffice::WelcomeController < UsersBackofficeController
     before_action :set_user
     
     def index
+        @user_statistic = UserStatistic.find_or_create_by(user: current_user)
     end
 
     def set_user
