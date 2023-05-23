407c66555962ec75ac3fb0004bdb1d97e7a6a02e
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-05-23 11:29:49 -0300

    Feat: Update profile imagem added

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -9,7 +9,11 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
     def update
         if @user.update(params_user)
             bypass_sign_in(@user)
-            redirect_to users_backoffice_profile_path, notice: "Usuário atualizado com sucesso!"
+            if params_user[:user_profile_attributes][:avatar]
+                redirect_to users_backoffice_welcome_index_path
+            else
+                redirect_to users_backoffice_profile_path, notice: "Usuário atualizado com sucesso!"
+            end
         else
             render :edit
         end
@@ -25,7 +29,7 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
     end
 
     def params_user
-        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, user_profile_attributes: [:address, :gender, :birthdate, :id])
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, user_profile_attributes: [:address, :gender, :birthdate, :id, :avatar])
       end
 
     def set_user
