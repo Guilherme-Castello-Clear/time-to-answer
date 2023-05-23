7856a086c323eb8251513ae3794caf7c48b60911
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-05-23 10:13:49 -0300

    Feat: Added extra data to user

--- app/controllers/users_backoffice/welcome_controller.rb
+++ app/controllers/users_backoffice/welcome_controller.rb
@@ -1,4 +1,10 @@
 class UsersBackoffice::WelcomeController < UsersBackofficeController
+    before_action :set_user
+    
     def index
+    end
+
+    def set_user
+        @user = User.find(current_user.id)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  has_one :user_profile
+  accepts_nested_attributes_for :user_profile, reject_if: :all_blank
+
+
+  validates :first_name, presence: true, length: { minimum: 3}, on: :update
 
   def full_name
-    "#{self.first_name} #{self.last_name}"
+      "#{self.first_name} #{self.last_name}"
+    end
   end
-end
