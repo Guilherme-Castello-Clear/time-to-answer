80a58a0db57cf126a8ab32dfc0b3eaaf979ebae2
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-05-26 09:01:33 -0300

    Feat: Question search improved with Elasticsearch

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   belongs_to :subject, counter_cache: true, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
-
+  searchkick
   after_create :set_statistic
 
 
